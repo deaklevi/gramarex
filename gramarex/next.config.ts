47b3d2c3c6bb4e48c99a9e0711824da1cf8b8685
Deák Levente <deakleventevok@gmail.com>
2026-08-17 19:16:44 +0200

    remove empty pages and set next.config.ts file

--- gramarex/next.config.ts
+++ gramarex/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',
+  // Opcionális: ha a képeket is szeretnéd statikusan kezelni,
+  // itt ki kell kapcsolni az alapértelmezett képoptimalizálást:
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
